b8c1dcab1e80bb8fede8efbf0fa83aa30fe370fb
Eric <zheng.eric.y@gmail.com>
2017-07-21 23:49:30 +0800

    add more languages

---
+++ hello.s
@@ -0,0 +1,19 @@
+section .text
+global _start
+_syscall:
+  int    0x80
+  ret
+_start:
+  push   dword     len
+  push   dword     msg
+  push   dword     1
+  mov    eax,      0x04
+  call   _syscall
+  add    esp,      0x0c
+  push   dword     0x00
+  mov    eax,      0x01
+  call   _syscall
+
+section .data
+  msg    db        "Hello World!",0x0a
+  len    equ       $ - msg
